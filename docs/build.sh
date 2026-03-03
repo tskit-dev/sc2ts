50fdf477bd488eb5fa0b6914b8c9c59ad5af9264
Jerome Kelleher <jk@well.ox.ac.uk>
2026-03-03 17:00:17 +0000

    Update

--- docs/build.sh
+++ docs/build.sh
@@ -1,12 +1,12 @@
 #/bin/bash
 
-# Jupyter-build doesn't have an option to automatically show the 
-# saved reports, which makes it difficult to debug the reasons for 
+# Jupyter-build doesn't have an option to automatically show the
+# saved reports, which makes it difficult to debug the reasons for
 # build failures in CI. This is a simple wrapper to handle that.
 
 REPORTDIR=_build/html/reports
 
-uv run --project=.. --group docs jupyter-book build . -vnW --keep-going
+uv run --project=../python --group docs jupyter-book build . -vnW --keep-going
 RETVAL=$?
 if [ $RETVAL -ne 0 ]; then
     if [ -e $REPORTDIR ]; then
